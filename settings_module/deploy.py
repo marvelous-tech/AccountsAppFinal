59b4962562a863be9bc7bcc9eb5a6831a6d773a2
dev-fahim <fahim6668@gmail.com>
2019-02-10 12:12:16 +0000

    Changed allowed hosts

--- settings_module/deploy.py
+++ settings_module/deploy.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['backend']
+ALLOWED_HOSTS = ['backend', 'localhost']
 
 
 # Application definition
