a03160f59b709955271e81d5e78ed0fdd1bff12e
Fahim Al Islam <34773501+dev-fahim@users.noreply.github.com>
2019-02-11 13:45:03 +0600

    changed media root

--- settings_module/deploy.py
+++ settings_module/deploy.py
@@ -125,8 +125,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-MEDIA_URL = '/blob/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/blob/')
 
 STATIC_URL = '/static/'
 
